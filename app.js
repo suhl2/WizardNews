89ee96e8070273e8145985ca7dbe1f63d29f02da
Sarah <sarah.celiann@gmail.com>
2023-02-23 11:13:27 -0600

    can now view individual posts

--- app.js
+++ app.js
@@ -4,21 +4,66 @@ const morgan = require("morgan");
 const postBank = require("./postBank");
 
 app.use(morgan('dev'));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
   const html = `<!DOCTYPE html>
   <html>
-  <head><title>Wizard News</title></head>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
   <body>
-    <ul>
-      ${posts.map(post => `<li>${post.title} by <span style="color: blue">${post.name}</span></li>`)}
-    </ul>
-  </body
-  </html>`
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`
   res.send(html);
 
 });
+
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+          <p>${post.content}</p>
+        </div>
+    </div>
+  </body>
+</html>`
+  res.send(html);
+})
 
 const PORT = 1337;
 
